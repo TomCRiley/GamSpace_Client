8a8f61894f5444fe7925a11c6fc33dc279870cc8
Tom Riley <tom.riley@outlook.com>
2022-04-21 20:49:32 +0100

    added endpoints for: create/edit/delete post

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,1 +1,43 @@
 import axios from 'axios';
+
+const baseurl = 'http://localhost:8000';
+
+export const createPost = async (id, post) => {
+  const options = {
+    method: 'POST',
+    url: `${baseurl}/posts/user/create/`,
+    data: post,
+    // headers: {
+    //   authorization: `Bearer ${window.sessionStorage.getItem('token')}`,
+    // },
+  };
+  const { data } = await axios.request(options);
+
+  return data;
+};
+
+export const editPost = async (id) => {
+  const options = {
+    method: 'PUT',
+    url: `${baseurl}/posts/user/update/`,
+    // headers: {
+    //   authorization: `Bearer ${window.sessionStorage.getItem('token')}`,
+    // },
+  };
+  const { data } = await axios.request(options);
+
+  return data;
+};
+
+export const deletePost = async (id) => {
+  const options = {
+    method: 'DELETE',
+    url: `${baseurl}/posts/user/update/`,
+    // headers: {
+    //   authorization: `Bearer ${window.sessionStorage.getItem('token')}`,
+    // },
+  };
+  const { data } = await axios.request(options);
+
+  return data;
+};
